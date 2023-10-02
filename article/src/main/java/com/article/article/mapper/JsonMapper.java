ea9b0de9d82eeb020c320bf57459ca500893ebcc
bmkim <mudosa1101@gmail.com>
2023-10-03 01:03:15 +0900

    뉴스로직 수정

--- article/src/main/java/com/article/article/mapper/JsonMapper.java
+++
@@ -1,8 +0,0 @@
-package com.article.article.mapper;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-
-public interface JsonMapper {
-    JsonNode readTree(String json) throws JsonProcessingException;
-}
