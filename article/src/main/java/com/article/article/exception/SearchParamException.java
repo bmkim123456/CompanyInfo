8e1b0fdb0ce9e7841cafabfbf08ac207c42013ed
bmkim <mudosa1101@gmail.com>
2023-10-10 00:26:16 +0900

    예외추가

---
+++ article/src/main/java/com/article/article/exception/SearchParamException.java
@@ -0,0 +1,21 @@
+package com.article.article.exception;
+
+public class SearchParamException extends RuntimeException{
+
+    private final int idSeq;
+    private final String companyName;
+
+    public SearchParamException (String message, int idSeq, String companyName) {
+        super(message);
+        this.idSeq = idSeq;
+        this.companyName = companyName;
+    }
+
+    public int getIdSeq() {
+        return idSeq;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+}
--- article/src/main/java/com/article/article/service/ArticleService.java
+++ article/src/main/java/com/article/article/service/ArticleService.java
@@ -3,6 +3,7 @@ package com.article.article.service;
 import com.article.article.dto.BigkindsResponse;
 import com.article.article.dto.CompanySearchParam;
 import com.article.article.dto.NaverResponse;
+import com.article.article.exception.SearchParamException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,7 +35,7 @@ public class ArticleService {
 
             } else log.info("기업명 {} 은(는) 사업 활동 중이 아닙니다.", searchParam.getCompanyName());
         } catch (RuntimeException e) {
-            throw new RuntimeException("오류가 발생하여 수집을 중단합니다.");
+            throw new SearchParamException("오류가 발생하여 수집을 중단합니다.", searchParam.getId_seq(), searchParam.getCompanyName());
         }
     }
 }
